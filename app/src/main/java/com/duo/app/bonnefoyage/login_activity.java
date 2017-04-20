131e17dc341724f942a6d9113a2f0b7f290b0845
Yannic Riesebos <zebybez@surfspot.nu>
2017-04-20 15:41:18 +0200

    fixed merge

--- app/src/main/java/com/duo/app/bonnefoyage/login_activity.java
+++ app/src/main/java/com/duo/app/bonnefoyage/login_activity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.duo.app.bonnefoyage.Activity.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
